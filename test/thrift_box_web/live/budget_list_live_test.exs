c3a844105c8303dcb61304cd7d90e48d6db32bd1
LidiyaShamalo <arkteya07@gmail.com>
2026-03-18 11:07:04 +0300

    refactor budget creation to use factory

--- test/thrift_box_web/live/budget_list_live_test.exs
+++ test/thrift_box_web/live/budget_list_live_test.exs
@@ -1,31 +1,24 @@
 defmodule ThriftBoxWeb.BudgetListLiveTest do
-  #alias ElixirLS.LanguageServer.Plugins.Phoenix
   #создает соединение, делая доступными функции регистрации и входа в систему
   use ThriftBoxWeb.ConnCase, async: true
-
   import Phoenix.LiveViewTest
-  import ThriftBox.TrackingFixtures
 
   alias ThriftBox.Tracking
-
   # доступность user для тестов
   setup do
-    user = ThriftBox.AccountsFixtures.user_fixture()
-
-    %{user: user}
+    %{user: insert(:user)}
   end
 
   describe "Index view" do
     test "shows budget when one exists", %{conn: conn, user: user} do
-      budget = budget_fixture(%{creator_id: user.id})
+      budget = insert(:budget, creator: user)
 
       conn = log_in_user(conn, user)
       {:ok, _lv, html} = live(conn, ~p"/budgets")
-
       #open_browser(lv)
-
       assert html =~ budget.name
       assert html =~ budget.description
+      assert html =~ user.name ###
     end
   end
 
@@ -44,12 +37,12 @@ defmodule ThriftBoxWeb.BudgetListLiveTest do
       conn = log_in_user(conn, user)
       {:ok, lv, _html} = live(conn, ~p"/budgets/new")
 
-      form = element(lv, "#create-budget-modal form")
+      form =
+        form(lv, "#create-budget-modal form", %{ #element(lv, "#create-budget-modal form")
+            "budget" => %{"name" => ""}
+      })
 
-      html =
-        render_change(form, %{
-          "budget" => %{"name" => ""}
-        })
+      html = render_change(form)
 
       assert html =~ html_escape("can't be blank")
     end
@@ -61,10 +54,8 @@ defmodule ThriftBoxWeb.BudgetListLiveTest do
       conn = log_in_user(conn, user)
       {:ok, lv, _html} = live(conn, ~p"/budgets/new")
 
-      form = form(lv, "#create-budget-modal form")
-
-      {:ok, _lv, html} =
-        render_submit(form, %{
+      form =
+        form(lv, "#create-budget-modal form", %{
           "budget" => %{
             "name" => "A new name",
             "description" => "The new description",
@@ -72,6 +63,9 @@ defmodule ThriftBoxWeb.BudgetListLiveTest do
             "end_date" => "2025-01-31"
           }
         })
+
+      {:ok, _lv, html} =
+        render_submit(form)
         |> follow_redirect(conn)
 
       #assert html =~ "Budget created"
@@ -82,8 +76,47 @@ defmodule ThriftBoxWeb.BudgetListLiveTest do
       assert created_budget.description == "The new description"
       assert created_budget.start_date == ~D[2025-01-01]
       assert created_budget.end_date == ~D[2025-01-31]
-
     end
   end
-end
 
+  test "validation errors are presentes when form is submitted with invalid input", %{
+    conn: conn,
+    user: user
+  } do
+    conn = log_in_user(conn, user)
+    {:ok, lv, _html} = live(conn, ~p"/budgets/new")
+
+    form =
+      form(lv, "#create-budget-modal form", %{
+        "budget" => %{"name" => ""}
+      })
+
+    html = render_submit(form)
+
+    assert html =~ html_escape("can't be blank")
+  end
+
+  test "end date before start date error is presented when form is submitted with invalid dates",
+      %{
+        conn: conn,
+        user: user
+      } do
+    conn = log_in_user(conn, user)
+    {:ok, lv, _html} = live(conn, ~p"/budgets/new")
+
+    attrs =
+      params_for(:budget,
+        start_date: ~D[2025-12-31],
+        end_date: ~D[2025-01-01]
+      )
+
+      form =
+      form(lv, "#create-budget-modal form", %{
+        budget: attrs
+      })
+
+    html = render_submit(form)
+
+    assert html =~ "must end after start date"
+  end
+end
